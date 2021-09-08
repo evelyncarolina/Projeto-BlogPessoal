e3ad3bba7627c6e00e72df2ba5e81a83580e01e5
evelyncarolina <evelyncarolina.contato@gmail.com>
2021-09-07 22:20:03 -0300

    [Feature Evelyn - Task: Criado UserDetailsServiceImplements]

--- blog/src/main/java/com/blogpessoal/blog/repositorio/UsuarioRepositorio.java
+++ blog/src/main/java/com/blogpessoal/blog/repositorio/UsuarioRepositorio.java
@@ -39,6 +39,8 @@ public interface UsuarioRepositorio extends JpaRepository<Usuario, Long> {
 	
 	Optional<Usuario> findByEmail(String email);
 
+	Optional<Usuario> findByUsuario(String userName);
+
 
 }
 
